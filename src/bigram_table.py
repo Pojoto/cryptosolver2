54a5044fa7d08a0ea325213330d2dadc8595828a
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-04-21 21:51:14 -0400

    set up str method and cleaned up init

--- src/bigram_table.py
+++ src/bigram_table.py
@@ -5,12 +5,15 @@ class bigram_table:
 
     def __init__(self, text):
 
-        self.matrix = bigramize(text)
+        self.matrix = self.bigramize(text)
 
         key = {}
-        init_key(key)
+        self.init_key(key)
 
         self.key = key
+    
+    def __str__(self):
+        return self.key
 
     
     def init_key(key):

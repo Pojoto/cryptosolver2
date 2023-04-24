6210727692b128b1d39ab2341df60ed3a7836a28
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-04-24 18:48:21 -0400

    relocated default frequency matrix

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,16 @@
 
-alphabet = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
-            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", 
+            ",", '"', "'", ";", ":"]
 
-default_freq_matrix = {}
+# default_freq_matrix = {}
 
-for letter1 in alphabet:
+# for letter1 in alphabet:
 
-    sub_dict = {}
+#     sub_dict = {}
 
-    for letter2 in alphabet:
-        sub_dict[letter2] = 0
+#     for letter2 in alphabet:
+#         sub_dict[letter2] = 0
     
-    default_freq_matrix[letter1] = sub_dict
+#     default_freq_matrix[letter1] = sub_dict
 

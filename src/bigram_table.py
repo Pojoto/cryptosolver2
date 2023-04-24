2f9a155c4f64bf0ab55ed656ed1494ca19881fa9
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-04-24 18:47:13 -0400

    evaluation and get_freq  function written and fixed bigramize

--- src/bigram_table.py
+++ src/bigram_table.py
@@ -1,42 +1,80 @@
 
-import constants
+from constants import alphabet
+
+def default_freq_matrix():
+
+    default_matrix = {}
+
+    for letter1 in alphabet:
+
+        sub_dict = {}
+
+        for letter2 in alphabet:
+            sub_dict[letter2] = 0
     
+        default_matrix[letter1] = sub_dict
+    return default_matrix
+    
+
 class Bigram_Table:
 
     def __init__(self, text):
 
-        text = text.upper()
+        text = text.lower()
 
         self.matrix = self.bigramize(text)
 
-        key = {}
-        self.init_key(key)
-
-        self.key = key
+        self.key = self.init_key()
     
     def __str__(self):
         return self.key
 
-    
-    def init_key(self, key):
-        for letter in constants.alphabet:
+    def init_key(self):
+        key = {}
+        for letter in alphabet:
             key[letter] = letter
         
-        #return key
+        return key
 
     def bigramize(self, text):
 
-        matrix = constants.default_freq_matrix
+        matrix = default_freq_matrix()
 
-        for i in range(len(text) - 1):
+        total = len(text) - 1
+
+        print(total)
+
+        for i in range(total):
             ch1 = text[i]
             ch2 = text[i + 1]
-            matrix[ch1][ch2] += 1
-        
+
+            ch1 = " " if ch1 == "\n" else ch1
+            ch2 = " " if ch2 == "\n" else ch2
+
+            matrix[ch1][ch2] += (1 / total)
+
         return matrix
 
-    def swap(self, i1, j1, i2, j2):
+    def get_freq(self, bigram):
+        bigram = bigram.lower()
+        ch1 = bigram[0]
+        ch2 = bigram[1]
+        return self.matrix[ch1][ch2]
+
+    def swap(self, key1, key2):
+
+        dict1 = self.matrix[key1]
+        dict2 = self.matrix[key2]
         
-        pass
+        for key in dict1.items():
+            dict1[key], dict2[key] = dict2[key], dict1[key]
+        
+        for dicts in self.matrix.values():
+            dicts[key1], dicts[key2] = dicts[key2], dicts[key1]
+
+                
+
+
+                    
 
 
--- src/main.py
+++ src/main.py
@@ -8,14 +8,26 @@ def main():
 
     cipher_table = Bigram_Table(ciphertext)
 
+    training_table = Bigram_Table(training)
+
+    # print(training_table.matrix)
+
     print(cipher_table.matrix)
 
-    print(cipher_table.matrix["T"]["H"])
-    print(cipher_table.matrix["L"]["K"])
+    print(cipher_table.get_freq("TH"))
+    print(cipher_table.get_freq("LK"))
+    print(cipher_table.get_freq("ZZ"))
+    print(cipher_table.get_freq("ZQ"))
 
-    print(ciphertext)
-    print(training)
+    print()
 
+    print(training_table.get_freq("TH"))
+    print(training_table.get_freq(" t"))
+    print(training_table.get_freq("lk"))
+    print(training_table.get_freq("zq"))
+    print(training_table.get_freq("zz"))
+
+    print("Eval:", evaluate(training_table, cipher_table))
 
     #this is a test comment
 
@@ -23,7 +35,15 @@ def main():
 
 
 
-
+def evaluate(table1, table2):
+    sum = 0
+    for dict_key in table1.matrix:
+        dict1 = table1.matrix[dict_key]
+        dict2 = table2.matrix[dict_key]
+        for key in dict1:
+            difference = (dict1[key] - dict2[key]) ** 2
+            sum += difference
+    return sum
 
 
 def text_from_txt(filename):

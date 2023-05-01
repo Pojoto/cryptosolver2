09516115cb7e2d67721d88205e5ce0f37218963e
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-04-30 22:30:52 -0400

    cleaned up constants

--- src/constants.py
+++ src/constants.py
@@ -3,14 +3,5 @@ alphabet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", 
             ",", '"', "'", ";", ":"]
 
-# default_freq_matrix = {}
 
-# for letter1 in alphabet:
-
-#     sub_dict = {}
-
-#     for letter2 in alphabet:
-#         sub_dict[letter2] = 0
-    
-#     default_freq_matrix[letter1] = sub_dict
 
